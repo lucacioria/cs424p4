ca4e272ab34137edaeea5209d3c533eefd57f893
claudio <caletti.claudio@gmail.com>
2012-11-10 16:11:03 -0600

    map

---
+++ src/com/project4/Application.java
@@ -0,0 +1,39 @@
+package com.project4;
+
+import com.anotherbrick.inthewall.EventSubscriber;
+import com.anotherbrick.inthewall.TouchEnabled;
+import com.anotherbrick.inthewall.VizNotificationCenter.EventName;
+import com.anotherbrick.inthewall.VizPanel;
+
+public class Application extends VizPanel implements TouchEnabled, EventSubscriber {
+
+  public Application(float x0, float y0, float width, float height) {
+    super(x0, y0, width, height);
+  }
+
+  @Override
+  public boolean touch(float x, float y, boolean down, TouchTypeEnum touchType) {
+    return propagateTouch(x, y, down, touchType);
+  }
+
+  @Override
+  public void setup() {
+    m.notificationCenter.registerToEvent(EventName.BUTTON_TOUCHED, this);
+    if (c.initializeVisualization) initializeVisualization();
+  }
+
+  private void initializeVisualization() {
+  }
+
+  @Override
+  public boolean draw() {
+    pushStyle();
+    popStyle();
+    return false;
+  }
+
+  @Override
+  public void eventReceived(EventName eventName, Object data) {
+  }
+
+}
